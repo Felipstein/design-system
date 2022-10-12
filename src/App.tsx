eecc240eb1f30ef25a85dd12524ab9e2a00d3952
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-10-11 23:29:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className='text-2xl font-bold'>Hello World</h1>
+
+      <button className='text-gray-900 text-lg font-semibold px-3 py-1.5 bg-cyan-500 hover:bg-cyan-300'>
+        Botão
+      </button>
+    </>
+  )
+}
